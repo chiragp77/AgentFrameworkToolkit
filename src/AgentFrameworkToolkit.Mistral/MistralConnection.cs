0a8dd2ed2f2fccd047d1d4b8407a0a07f519abcf
Rasmus Wulff Jensen <rasmus.wulff.jensen@gmail.com>
2025-11-22 09:37:17 +0100

    WIP

--- src/AgentFrameworkToolkit.Mistral/MistralConnection.cs
+++ src/AgentFrameworkToolkit.Mistral/MistralConnection.cs
@@ -1,6 +1,12 @@
 ﻿namespace AgentFrameworkToolkit.Mistral;
 
+/// <summary>
+/// Represents a connection to the Mistral API
+/// </summary>
 public class MistralConnection
 {
+    /// <summary>
+    /// The API Key for connecting to Mistral
+    /// </summary>
     public required string ApiKey { get; set; }
 }
